2576a6331bb7aa3fbbb933b08116fde8993242cb
Placido81 <mark.olivo@bryanuniversity.edu>
2020-12-29 11:09:47 -0500

    practice assignment

--- week2/creating appending_elements/index.js
+++ week2/creating appending_elements/index.js
@@ -11,14 +11,30 @@ var p = document.createElement("p")
 p.textContent = "All of this was created with Javascript"
 body.append(p)
 
-var ol = document.createElement("ol")
-ol.setAttribute ("li")
-var li = document.createElement("li")
 
-document.body.appendChild(ol,li)	
 
-var ol = document.createElement("ol")
+
+
+
 
 var li = document.createElement("li")
-li.textContent = "Love learning"
-document.body.append(ol,li)
+li.textContent = "Love Learning"
+document.body.append(li)
+
+var li = document.createElement("li")
+li.textContent = "Coding Is Fun"
+document.body.append(li)
+
+var li = document.createElement("li")
+li.textContent = "Learn To Code"
+document.body.append(li)
+
+var li = document.createElement("li")
+li.textContent = "Coding Can Be Tricky"
+document.body.append(li)
+
+
+
+
+
+
