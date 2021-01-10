5306ed29e4061cae1c063b8b2dec08eb7de5fb85
Placido81 <mark.olivo@bryanuniversity.edu>
2021-01-10 17:19:59 -0500

    week3 practice

---
+++ week3/changing_styles_wjs/index.js
@@ -0,0 +1,43 @@
+// H2 list & Style:
+var h2 = document.createElement("h2")
+h2.textContent = "Syntax"
+document.body.append(h2);
+h2.style.fontSize = "20px";
+h2.style.fontWeight = "lighter";
+h2.style.fontFamily = "sans-serif"
+h2.style.color = "cornflowerblue";
+document.querySelector("#javascript").classList.add("border");
+
+var h2 = document.createElement("h2")
+h2.textContent = " For Loop"
+document.body.append(h2);
+h2.style.fontSize = "20px";
+h2.style.fontWeight = "lighter";
+h2.style.fontFamily = "sans-serif"
+h2.style.color = "cornflowerblue";
+
+var h2 = document.createElement("h2")
+h2.textContent = "Functions"
+document.body.append(h2);
+h2.style.fontSize = "20px";
+h2.style.fontWeight = "lighter";
+h2.style.fontFamily = "sans-serif"
+h2.style.color = "cornflowerblue";
+
+var h2 = document.createElement("h2")
+h2.textContent = "Boolean"
+document.body.append(h2);
+h2.style.fontSize = "20px";
+h2.style.fontWeight = "lighter";
+h2.style.fontFamily = "sans-serif"
+h2.style.color = "cornflowerblue";
+
+var h2 = document.createElement("h2")
+h2.textContent = "If Statement"
+document.body.append(h2);
+h2.style.fontSize = "20px";
+h2.style.fontWeight = "lighter";
+h2.style.fontFamily = "sans-serif"
+h2.style.color = "cornflowerblue";
+
+
