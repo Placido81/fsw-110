fa780f01be44533c5a26d5a29ce4b56fb0396dca
Placido81 <mark.olivo@bryanuniversity.edu>
2021-01-16 10:47:09 -0500

    practice

--- week4/AddEventListner/index.js
+++ week4/AddEventListner/index.js
@@ -1,26 +1,28 @@
-/* var square1 = document.getElementById("square1").addEventListener("mouse over", function(){ */
-/* document.body.style.backgroundColor="blue"; */
-/* document.getElementById("square1").style.border= "2px solid black"; */
-/* square1.addEventListener("click", function(){ */
-/*     console.log("hello") */
-/* }) */
+// Header:
+var h1 = document.createElement("h1")
+h1.textContent = "DJ.JS"
+document.body.append(h1)
+// Button ("Click Me"):
 var button = document.getElementById("button");
 
-// button.addEventListener("mouse over", function(){
-// console.log(" button has been clicked!")
-// })
-// })
-
-button.addEventListener("click", function(){
-    console.log("hello")
-    button.style.color = "red"
-})
-
+// Square with diffrent Functions:
 var square1 = document.getElementById("square1")
 square1.style.height = "300px"
 square1.style.width = "300px"
-square1.style.backgroundColor ="blue"
-
-square1.addEventListener('mouseover', function(){
-    square1.style.backgroundColor = "red"
+// Styling Square with color:
+square1.addEventListener("mouseover", function(){
+square1.style.backgroundColor ="blue";
 })
+square1.addEventListener("mouseout", function(){
+    square1.style.backgroundColor = "red";
+})
+square1.addEventListener("mouseup", function(){
+    square1.style.backgroundColor = "yellow";
+})
+square1.addEventListener("mousedown", function(){
+    square1.style.backgroundColor = "green";
+})
+square1.addEventListener("mousewheel", function(){
+   square1.style.backgroundColor = "orange";
+})
+
