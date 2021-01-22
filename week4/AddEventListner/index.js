3a320b41e1180ad10e567285bb27e062c479974a
Placido81 <mark.olivo@bryanuniversity.edu>
2021-01-22 13:25:17 -0500

    resubmit

--- week4/AddEventListner/index.js
+++ week4/AddEventListner/index.js
@@ -8,14 +8,14 @@ var square1 = document.getElementById("square1")
 square1.style.height = "300px"
 square1.style.width = "300px"
 // Styling Square with color:
+
 square1.addEventListener("mouseover", function(){
 square1.style.backgroundColor ="blue";
 })
-function myFunction(){
-    alert("press any key to change color.")
 
 square1.addEventListener("mouseout", function(){
     square1.style.backgroundColor = "red";
+    
     
     
   
@@ -29,4 +29,21 @@ square1.addEventListener("mousedown", function(){
 square1.addEventListener("mousewheel", function(){
    square1.style.backgroundColor = "orange";
 })
-}
+document.addEventListener("keydown", function(event) {
+    if(event.which === 66 ){
+    square1.style.backgroundColor = "blue";
+    }
+    if (event.which === 82){
+        square1.style.backgroundColor= "red"
+    }
+    if (event.which === 89){
+        square1.style.backgroundColor= "yellow"
+    }
+    if (event.which === 71){
+        square1.style.backgroundColor= "green"
+    }
+    if (event.which === 79){
+        square1.style.backgroundColor= "orange"
+    }
+  })
+  
