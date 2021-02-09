7239cd2527b862aff6a80b466045c53cf76e3a65
Placido81 <mark.olivo@bryanuniversity.edu>
2021-02-09 09:44:02 -0500

    To Do List

--- week7/capStoneProj/index.js
+++ week7/capStoneProj/index.js
@@ -1,30 +1,56 @@
 
-  function ValidationEvent() {
+ var myNodelist = document.getElementsByTagName("LI");
+ var i;
+ for (i = 0; i < myNodelist.length; i++) {
+   var span = document.createElement("SPAN");
+   var txt = document.createTextNode("\u00D7");
+   span.className = "close";
+   span.appendChild(txt);
+   myNodelist[i].appendChild(span);
+ }
+ 
+ // Click on a close button to hide the current list item
+ var close = document.getElementsByClassName("close");
+ var i;
+ for (i = 0; i < close.length; i++) {
+   close[i].onclick = function() {
+     var div = this.parentElement;
+     div.style.display = "none";
+   }
+ }
+ 
+ // Add a "checked" symbol when clicking on a list item
+ var list = document.querySelector('ul');
+ list.addEventListener('click', function(ev) {
+   if (ev.target.tagName === 'LI') {
+     ev.target.classList.toggle('checked');
+   }
+ }, false);
+ 
+ // Create a new list item when clicking on the "Add" button
+ function newElement() {
+   var li = document.createElement("li");
+   var inputValue = document.getElementById("myInput").value;
+   var t = document.createTextNode(inputValue);
+   li.appendChild(t);
+   if (inputValue === '') {
+     alert("You must write something!");
+   } else {
+     document.getElementById("myUL").appendChild(li);
+   }
+   document.getElementById("myInput").value = "";
+ 
+   var span = document.createElement("SPAN");
+   var txt = document.createTextNode("\u00D7");
+   span.className = "close";
+   span.appendChild(txt);
+   li.appendChild(span);
+ 
+   for (i = 0; i < close.length; i++) {
+     close[i].onclick = function() {
+       var div = this.parentElement;
+       div.style.display = "none";
+     }
+   }
+ }
     
-    var name = document.getElementById("name").value;
-    var list = document.getElementById("list").value;
-    var contact = document.getElementById("contact").value;
-    
-    
-    // Conditions
-    if (name != '' && list != '' && contact != '') {
-    if (list.match(listReg)) {
-    if (document.getElementById("day").checked || document.getElementById("night").checked) {
-    if (contact.length == 10) {
-    return true;
-    } else {
-    return false;
-    }
-    } else {
-    alert("You must select DayTime.....!");
-    return false;
-    }
-    } else {
-    alert("Or NightTime...!");
-    return false;
-    }
-    } else {
-    return false;
-    }
-    }
-  
